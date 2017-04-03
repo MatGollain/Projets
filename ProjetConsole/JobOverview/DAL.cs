57d0f98fa20bd26b4722fd0ac7ebcbe9478df57d
vportemer <vportemer@VPORTEMER17-DE>
2017-04-03 10:38:09 +0200

    projet début

---
+++ ProjetConsole/JobOverview/DAL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JobOverview
+{
+    class DAL
+    {
+        public void ChargerFichier()
+        {
+            string chemin = @"..\..\DonnéesMétéoParis.txt";
+
+            int cpt = 0;
+            using (StreamReader str = new StreamReader(chemin))
+            {
+                string ligne;
+
+                while ((ligne = str.ReadLine()) != null)
+                {
+                    cpt++;
+                    if (cpt == 1) continue; // On n'analyse pas la première ligne car elle contient les en-têtes
+
+                    var tab = ligne.Split('\t');
+                    try
+                    {
+                        var donnéesMois = new DonnéesMois
+                        {
+                            Mois = DateTime.Parse(tab[0]),
+                            TMin = double.Parse(tab[1]),
+                            TMax = double.Parse(tab[2]),
+                            Précipitations = double.Parse(tab[3]),
+                            Ensoleillement = double.Parse(tab[4])
+                        };
+
+                        // Ajout des données du mois à la liste
+                        Data.Add(donnéesMois);
+                    }
+                    catch (FormatException)
+                    {
+                        // On ignore simplement la ligne
+                        Console.WriteLine("Erreur de format à la ligne suivante :\r\n{0}", ligne);
+                    }
+                }
+}
---
+++ ProjetConsole/JobOverview/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobOverview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new ChargeFichier();
+            a1.ChargerFichier();// appele  charger fichier et charge les données
+
+        }
+    }
+}
