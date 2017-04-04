26d57a5d2d7442f4c7ce06b6ca3e98281667ccb0
vportemer <vportemer@VPORTEMER17-DE>
2017-04-04 11:48:19 +0200

    mardi12h

--- ProjetConsole/JobOverview/Results.cs
+++ ProjetConsole/JobOverview/Results.cs
@@ -6,45 +6,42 @@ using System.Threading.Tasks;
 
 namespace JobOverview
 {
-    class Results
+    public static class Results
     {
-        public void CalculerTravailRéalisé(List<Taches> p)
-        {
 
-        }
-        // méthodes de calcules / affichage? 
-        /*
-        CalculerTempsTotal
-            => Surcharge en fonction du besoin / personne / version / métier etc
-        CalculerTempsActivité
-        CalculerTempsMétierRéalisé
-        CalculerTempsMétierRestant
-        CalculerTempsMétierTotal
-        CalculerTempsPersonneRéalisé
-        CalculerTempsPersonneRestant
-        CalculerTempsPersonneTotal
-        */
 
-        public int DuréeTotaleActivité(List<Production> data, CodeActivités act)//Calcul de la durée prévue totale par activité. On appelle en paramètre
+        public static int DuréeTotaleActivité(List<Production> data, CodeActivités act)//Calcul de la durée prévue totale par activité. On appelle en paramètre
         {
             var preAct = data.Where(a => a.ActivitéTache == act).Sum(dtp => dtp.DuréeTravailPrévu);
             return preAct;
         }
 
-        public int DuréeTravailRéaliséPersonne(List<Production> data, string codper, string ver)
+        public static int DuréeTravailRéaliséPersonne(List<Production> data, string codper, string ver)
         {
             return data.Where(p => p.CodePersonne == codper && p.Version == ver).Sum(dtr => dtr.DuréeTravailRéalisé);
         }
 
-        public int DuréeTravailRestantPersonne(List<Production> data, string codper, string ver)
+        public static int DuréeTravailPrévuPersonne(List<Production> data, string codper, string ver)
+        {
+            return data.Where(p => p.CodePersonne == codper && p.Version == ver).Sum(dtp => dtp.DuréeTravailPrévu);
+        }
+
+        public static int DuréeTravailRestantPersonne(List<Production> data, string codper, string ver)
         {
             return data.Where(p => p.CodePersonne == codper && p.Version == ver).Sum(dtres => dtres.DuréeTravailRestant);
         }
-        public int DuréeTravailAnnexe(List<Annexes>data, int mois, string nomtache)
+        public static int DuréeTravailAnnexe(List<Annexes> data, int mois, string nomtache)
         {
-            return data.Where(p => p.DateAnnexe.Month == mois && p.LibTache==nomtache).Sum(dtr => dtr.DuréeTravailRéalisé);
+            return data.Where(p => p.DateAnnexe.Month == mois && p.LibTache == nomtache).Sum(dtr => dtr.DuréeTravailRéalisé);
         }
-
+        public static int DuréeTravailPrévuVersion(List<Production> data, string ver)
+        {
+            return data.Where(p => p.Version == ver).Sum(dtp => dtp.DuréeTravailPrévu);
+        }
+        public static int DuréeTravailRéaliséVersion(List<Production> data, string ver)
+        {
+            return data.Where(p => p.Version == ver).Sum(dtr => dtr.DuréeTravailRéalisé);
+        }
 
     }
 }
